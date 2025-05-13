b0c8ffbb0ad4afa565125ec33e27efa1ef9011a3
하용군 <hay_1845@naver.com>
2025-05-13 12:34:29 +0900

    SQL 최초 등록 order by 추가

---
+++ workspace_sql/Oracle.sql
@@ -0,0 +1,27 @@
+-- 한줄 주석
+/* 범위 주석*/
+/*여러줄
+  주석 
+  */
+-- SQL : 
+select * from emp;
+select * from dept;
+select * from salgrade;
+select empno, deptno
+from emp;
+select ename, sal,
+123*7, 'a' 
+from emp;
+select deptno 
+from emp;
+select distinct deptno 
+from emp;
+
+select distinct job, deptno 
+from emp;
+select ename "2름", sal 월급, 
+sal "1번", sal*12+comm  감,
+sal*12+comm 돈,
+sal*12+comm "월 급", -- sql에서 유일하게" 들어가는 곳 컬럼에 이름정하는것
+comm as 추가수당
+from emp;
