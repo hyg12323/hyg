33283150af33360d3272c457241e221f6de18e1a
하용군 <hay_1845@naver.com>
2025-06-05 19:20:45 +0900

    배열 학습및 문제풀이

--- workspace_java/chap05/src/sec01/exam01/ArrayExam.java
+++ workspace_java/chap05/src/sec01/exam01/ArrayExam.java
@@ -102,11 +102,11 @@ public class ArrayExam {
 	       
 	       
 	       
-	       int[] [] bs = new int[10][3];
+	       int[] [] bs = new int[2][3];
 	       bs[0] = bs0; 
 	       bs[1] = bs1; 
 	        
-	      
+	      System.out.println("bs.length:"+bs0.length);
             
 //          for(int i =1; i<=10; i++) {
 //          }
@@ -129,6 +129,11 @@ public class ArrayExam {
         
           for(int c1 =1; c1<c[0]; c1++)
         	  System.out.println(c1);
+          
+          
+          
+          
+          
 //             
 //          for(int i =1; i<=10; i++)
 //        	  
@@ -138,16 +143,291 @@ public class ArrayExam {
 //          c[0] = i;
         		  
 //          
-	
+          
+          int[] d1 = {1,2,3,4,5};
+          int[] d2 = d1; //주소 동일하게 만듬
+          int[] d3 = {1,2,3,4,5};
+          System.out.println("d2 == d1:" +(d1 == d2));
+          System.out.println("d2 == d1:" +(d1 == d3));
+	      
+          d2[1] = 10; // 2를 10으로 변경 
+          System.out.println("d1:"+d1[1]); // 주소가 같으므로 d2를 바꾸면 d1도 바뀜
 
-	
+                     	
+           
+         int[] f = {4,5,7,2,6,9};
+         int[] f2= new int[f.length]; // length는 f안에 방의 개수 
+  //        System.out.println(f2[1]);
+//       
+          
+    //      for(int f2=0; f2<=f.length; f2++) {
+      //  	 System.out.println(f[f2]);
+        	 
+         
+         
+         
+	     System.out.println(f[0]);
+	     for(int i=0; i<f.length; i++) { 
+	    	 System.out.println(f[i]);        //i는 방에 개수 0부터 새주기
+	    	 f2[i] =f[i];
+	    	 System.out.println(f2[i]); 
+	     } 
+	     
+	      
+	   for(int i=0; i<f.length; i++) {
+		   int data = f[i];         
+		   System.out.println("data:"+data);
+	   }
+	   //향상된 for문
+	   for(int data :f) {
+		   System.out.println("data:"+data);
+	   } 
+	   
+	   //문제 0
+	   // 배열 깊은 복사 : {1,2,3} -> {1,2,3}
+	   int[]ac = new int[3];
+	   ac[0]= 1;
+	   ac[1]= 2;
+	   ac[2]= 3;
+	  
+	   for(int c1= 0; c1<3; c1++) {
+	   System.out.print(ac[c1]);
+	   }
+	   
+	   System.out.println();
+	   
+	   int[] b = {1,2,3};
+	   for(int c2= 0; c2<3; c2++) {
+		   System.out.print(b[c2]);
+		   }
+	   
+	   
+	   System.out.println();
+	   //문제1 
+	  // 배열 뒤집기 : {1,2,3,} -> {3,2,1,}
+	   
+	   for(int c3= 2; c3>=0; c3--) {
+		   System.out.print(ac[c3]);
+	   }
+          		   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   //문제2
+	   // [3,4,7,5,1,4,9]
+	   //여기서 홀수의 개수 찾기
+	   System.out.println();
+	   int[] aw = {3,4,7,5,1,9,4};
+	   int count = 0;
+	      
+	   for(int i=0; i<aw.length; i++) {
+		
+	         
+		 if(aw[i]%2!=0) {
+			count++;
 
-	
-	
-	
-	
-	
-	
+			 System.out.println(aw[i]);
+ 			 
+		 }
+		 
+	   }//System.out.println("2번:"+count);
+	    
+	   
+	   
+	   
+	   
+	   
+	   count = 0;
+	   
+	   
+	   //문제3
+	   //[3,4,7,5,1,4,9] 4보다 큰 수의 개수 구하기
+	      
+	   for(int i=0; i<aw.length; i++) {
+		
+	         
+		 if(aw[i]>4) {
+			 count++;
+		 }
+			
+	   }  System.out.println("3번"+count);
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   //문제4 
+	   //여기서 최대값을 출력하시오
+	   
+	  int max = 0;
+	      
+	   for(int i=0; i<aw.length; i++) {
+		   
+	        
+		 if(aw[i]>max) {
+			 max = aw[i];	
+		
+		 } 
+	   }System.out.println("4번"+max);
+	   
+	   
+	   
+	   
+	   
+	   
+       	   
+	   //문제5
+	   //여기서 두번째 큰 수 구하기
+	   
+	   
+	      
+	   for(int i=0; i<aw.length; i++) {
+		   
+	        
+		 if(aw[i]<=max) {
+	       max = aw[i];  
+					 
+		
+		 } 
+	   }System.out.println("ww:"+max);	
+	   
+	   
+	  
+	   
+	   
+	   
+	   
+			   
+	   //문제6		   
+	   //오른쪽으로 한칸밀기(왼쪽은 0으로 채우기)
+	   //예시 [0,3,7,5,1,9,4]  배열하나가 늘어남
+	   
+	   
+	 //  int[] aw[i] = {3,4,7,5,1,9,4};
+        
+	   int[] w = new int[aw.length+1];
+	   
+	   for(int ie=0; ie<aw.length; ie++) {
+		   w[ie+1] = aw[ie];
+        	 System.out.println("6번"+w[ie]);
+        //	System.out.println(w[ie]);
+        	
+        } 
+	    
+
+	    
+	   
+	   
+	  // int[][] w = new int[2][6];
+	//     w[0] = aw;
+	     
+	     
+	   
+	   
+	   
+	   
+	   
+	   
+	   //문제7
+	   // 오른쪽으로 이동 하는데 맨 끝에 값을 맨 처음으로 보내기
+	   //{3,4,7,5,1,9,4}  -> {4,3,4,7,5,1,9}
+	   
+	   
+            
+	   int[] w2  = new int [aw.length-1];
+         w2[0] = aw.length-1;
+	   for(int ii=1; ii<w2.length; ii++) {
+	     w2[ii] =aw[ii-1];
+	             
+		     
+	        System.out.println(w2[ii]);
+		   
+	   }
+
+	   
+        	
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   //문제8
+	   //임시 비밀번호 생성(랜덤)
+	   // 8-1 : 숫자만
+	   // 8-2 : 소문자만
+	   // 8-3 : 숫자2개 이상, 대/소문자 각 1개 이상
+	   
+	   //문제9
+	   //자리가 10개 있는 소극장의 예약 시스템
+       // 자리 번호는 1~10번까지 번호의 자리가 있습니다
+	   // 메뉴 : "1.예약 2.모든 좌석 현황" 3. 잔여좌석 0.종료
+	   //만약1 : 예약이 가능하다면 "n번 자리 예약 했습니다"
+	   //만약2 : 예약이 불가능하다면 "이미 예약 되어 있습니다"
+	   
+	   
+	   //문제10
+	   // 로또 6개 생성. 단, 중복 없이
+	   
+	   
+	   int [] g =new int[6];
+	   
+//	   g[0] = (int)(Math.random()*45+1);
+//	   g[1] = (int)(Math.random()*45+1);
+//	   g[2] = (int)(Math.random()*45+1);
+//	   g[3] = (int)(Math.random()*45+1);
+//	   g[4] = (int)(Math.random()*45+1);
+//	   g[5] = (int)(Math.random()*45+1);
+	   for(int i=0; i<g.length; i++) {
+		   g[i] = (int)(Math.random()*45+1);
+		   System.out.println("로또"+g[i]);  
+		for(int cc= 0; cc<=i; cc++) { 
+			if (g[i] != g[cc]) {
+				System.out.println();	
+				
+			}
+		            	
+			//if(g[i] g[cc])  {             // 중복이 안됬다
+			
+					
+			//}
+			
+		 
+			
+		}
+		
+		
+		   
+	   }  
+		   
+		      
+	   
+	   
+	   
+	   
+	     
+	    
+//	   for(int i=1; i<46; i++) {
+//		   int[] c2 = new int[i];
+//		   
+		   
+	   
+	   
+	   
+	   
+	   
+	   
 	}
 	
 	
