85dc0e6299c56a075d2680dd00f9f2359efe8333
하용군 <hay_1845@naver.com>
2025-08-14 17:30:50 +0900

    오늘 문제풀이

---
+++ workspace_java/proj2_request/src/main/java/sec01/exam01/loginSevlet.java
@@ -0,0 +1,63 @@
+package sec01.exam01;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/qulz/login")
+public class loginSevlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+   
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        System.out.println("login 실행");
+        request.getRequestDispatcher("/qulz/login.html").forward(request, response);
+        
+        
+    }
+
+    
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+       
+
+        String id = request.getParameter("id1");
+        String pw = request.getParameter("pw1");
+
+        System.out.println("login 실행 post");
+
+        try {
+//        	int n1 = Integer.parseInt(id);
+//            int n2 = Integer.parseInt(pw);
+              if(id == ""|| pw =="") {
+            	  System.out.println("id 또는 비밀번호 확인바랍니다");
+//            	  System.out.println("비번"+pw);
+              }
+              else {
+              System.out.println("id: " + id);
+              System.out.println("pw: " + pw);
+              }
+              
+//              response.sendRedirect(request.getContextPath() + "/qulz/login")
+              
+              
+              if("admin".equals(id)&&"1234".equals(pw)) {
+            	  response.sendRedirect(request.getContextPath() + "/qulz/admin.html");
+            	  return;
+              }
+           
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+   
+        response.sendRedirect(request.getContextPath() + "/qulz/login.html");
+    }
+}
